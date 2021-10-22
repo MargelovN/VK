b176876da4921e4b820b55180ebc1289fce8fc99
MargelovN <margelovn@gmail.com>
2021-10-22 16:20:52 +0900

    V1

---
+++ src/main/java/ru/netology/domain/VK/VkPost.java
@@ -0,0 +1,13 @@
+package ru.netology.domain.VK;
+
+public class VkPost {
+    private int id;
+    private int author;
+    private int date;
+    private int profilePicture;
+    private int likes;
+    private int commentsInfo;
+    private int reposts;
+    private int views;
+    // + get/set на все поля
+}
